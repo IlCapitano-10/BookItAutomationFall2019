b088b0eabccff6e1f09b29cb07e4858ead224c27
Astrit <astrit989@gmail.com>
2020-05-31 16:47:57 -0500

    first comm

--- src/test/java/com/bookit/step_definitions/SelfStepDefinitions.java
+++ src/test/java/com/bookit/step_definitions/SelfStepDefinitions.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 public class SelfStepDefinitions {
     SelfPage selfPage = new SelfPage();
-    UserDB userDB=new UserDB();
+    UserDB userDB = new UserDB();
 
     /**
      *  Then user verifies that information displays correctly:
@@ -20,15 +20,18 @@ public class SelfStepDefinitions {
      */
     @Then("user verifies that information displays correctly:")
     public void user_verifies_that_information_displays_correctly(List<Map<String, String>> dataTable) {
-        System.out.println("##############[ui validations]###################");
+        System.out.println("###############[UI validations]###############");
+
         String fullName = dataTable.get(0).get("first-name") +" "+dataTable.get(0).get("last-name");
         Assert.assertEquals(fullName, selfPage.getUserInfo("name"));
         Assert.assertEquals(dataTable.get(0).get("role"), selfPage.getUserInfo("role"));
         Assert.assertEquals(dataTable.get(0).get("campus-location"), selfPage.getUserInfo("campus"));
         Assert.assertEquals(dataTable.get(0).get("batch-number"), selfPage.getUserInfo("batch").replace("#", ""));
         Assert.assertEquals(dataTable.get(0).get("team-name"), selfPage.getUserInfo("team"));
-        System.out.println("##############[Database validations]###################");
+
+        System.out.println("###############[Database validations]###############");
 
         Assert.assertTrue(userDB.checkIfUserExistInDB(dataTable.get(0).get("email")));
+
     }
 }
